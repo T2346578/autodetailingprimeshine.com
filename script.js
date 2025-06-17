fc3fe4e22d8b140ca334069349344abb50d385ae
T2346578 <lprimeshinel@gmail.com>
2025-06-17 14:42:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -75,9 +75,9 @@ document.addEventListener('DOMContentLoaded', function() {
             comment: "First time using Prime Shine. Trevor was polite and honest and incredibly hard working. My car looks brand new!!!! His attention to detail is impeccable. I will continue to call Derek in the future, when I get my car out of winter storage. Great job 👏 well done and keep up the great work 👍.",
             image: "assets/paultest.png",
             carImages: [
-                "assets/redcivic1.jpg",
-                "assets/redcivic2.jpg",
-                "assets/redcivic3.jpg",
+                "assets/redcivic1.JPG",
+                "assets/redcivic2.JPG",
+                "assets/redcivic3.JPG",
             ]
         },
         {
@@ -86,7 +86,7 @@ document.addEventListener('DOMContentLoaded', function() {
             comment: "I recently hired Trevor to do a 2 stage paint correction with a ceramic coating on my newly purchased black car and now my car looks amazing. I was very impressed by Trevor's professionalism and how detail oriented he was while he worked on my car. This guy clearly knows what he's doing, and I'll definitely be calling him again with any of my auto detailing needs. Give him a call, won't be disappointment. Thank you Trevor for making my car look amazing!",
             image: "assets/michaletest.png",
             carImages: [
-                "assets/blackmercedes.jpg"
+                "assets/blackmercedes.JPG"
             ]
         },
         {
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', function() {
             rating: 5,
             comment: "I had them do the ceramic coating and detailing on my Miata NB, and the results were incredible. No exaggeration—it looked like a brand new car.",
             image: "assets/erictest.png",
-            carImage: "assets/redmiata.jpg"
+            carImage: "assets/redmiata.JPG"
         },
         {
             name: "Emily White",
